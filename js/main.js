e22bacdf026181e6172f9ae272236183c2cc189c
Francesco Pagano <fra.pgn@protonmail.com>
2020-02-29 18:10:43 +0100

    icon rotation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
 
     $('.menu-item').click(function(){
+        // dropdown-menu visibility
         if ($(this).find('.dropdown-menu').is(':hidden')) {
             $('.menu-item').find('.dropdown-menu').slideUp(300);
             $(this).find('.dropdown-menu').slideToggle(300);
+            //icon rotation
+            $(this).siblings('.menu-item').children('a').removeClass('rotate');
+            $(this).children('a').toggleClass('rotate');
         } else {
             $(this).find('.dropdown-menu').slideToggle(300);
+            //icon rotation
+            $(this).children('a').toggleClass('rotate');
         }
-    });
 
-    $('.menu-item').click(function(){
+        // menu-item  indicator
         $(this).addClass('active');
         $(this).siblings('.menu-item').removeClass('active');
-    })
+
+        // icon rotation
+
+
+    });
+
 
 });

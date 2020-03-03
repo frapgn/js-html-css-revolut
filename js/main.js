4c285122b762732beb315124338fc1526ce27c64
Francesco Pagano <fra.pgn@protonmail.com>
2020-03-03 15:08:34 +0100

    aggiunto codice parlante

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
         // dropdown-menu visibility
         if ($(this).find('.dropdown-menu').is(':hidden')) {
             $('.menu-item').find('.dropdown-menu').slideUp(300);
-            $(this).find('.dropdown-menu').slideToggle(300);
+            $(this).find('.dropdown-menu').slideDown(300);
             //icon rotation
             $(this).siblings('.menu-item').children('a').removeClass('rotate');
-            $(this).children('a').toggleClass('rotate');
+            $(this).children('a').addClass('rotate');
         } else {
-            $(this).find('.dropdown-menu').slideToggle(300);
+            $(this).find('.dropdown-menu').slideUp(300);
             //icon rotation
-            $(this).children('a').toggleClass('rotate');
+            $(this).children('a').removeClass('rotate');
         }
 
         // menu-item  indicator
